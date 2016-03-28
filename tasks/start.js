fcf7aabe56f969802b9efe76457333af157d2426
demohi <mds@xue.bi>
2016-03-28 14:53:42 +0800

    bump 0.2.0 & add params

--- tasks/start.js
+++ tasks/start.js
@@ -1,16 +1,20 @@
 var path = require('path');
 var utils = require('shipit-utils');
+var util = require('util');
 
 module.exports = function(shipit) {
   utils.registerTask(shipit, 'pm:start', task);
   function task() {
     shipit.config = shipit.config || {};
     shipit.config.pm = shipit.config.pm || {};
-    var config = shipit.config.pm[shipit.options.environment];
-    if(!config){
+    var config = shipit.config.pm[shipit.options.environment] || {};
+    if(!config.path){
       throw new Error('you need to add shipit-pm config');
     }
-
-    return shipit.remote('pm2 startOrRestart ' + config);
+    var params = config.params || '';
+    if(params){
+      params = util.format('%s &&', params);
+    }
+    return shipit.remote(util.format('%s pm2 startOrRestart %s', params, config.path));
   }
 }
