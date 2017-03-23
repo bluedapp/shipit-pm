d950d8807b2bfd08ad93856bba1442bf1a33da2f
xiaoyu <306766053@qq.com>
2017-03-23 19:08:15 +0800

    add new config

--- tasks/start.js
+++ tasks/start.js
@@ -1,20 +1,22 @@
-var path = require('path');
-var utils = require('shipit-utils');
-var util = require('util');
+'use strict'
 
-module.exports = function(shipit) {
-  utils.registerTask(shipit, 'pm:start', task);
-  function task() {
-    shipit.config = shipit.config || {};
-    shipit.config.pm = shipit.config.pm || {};
-    var config = shipit.config.pm[shipit.options.environment] || {};
-    if(!config.path){
-      throw new Error('you need to add shipit-pm config');
+var utils = require('shipit-utils')
+var util = require('util')
+
+module.exports = function (shipit) {
+  utils.registerTask(shipit, 'pm:start', task)
+  function task () {
+    shipit.config = shipit.config || {}
+    shipit.config.pm = shipit.config.pm || {}
+    var config = shipit.config.pm[shipit.options.environment] || {}
+    if (!config.path) {
+      throw new Error('you need to add shipit-pm config')
     }
-    var params = config.params || '';
-    if(params){
-      params = util.format('%s &&', params);
+    var options = config.options || ''
+    var params = config.params || ''
+    if (params) {
+      params = util.format('%s &&', params)
     }
-    return shipit.remote(util.format('%s pm2 startOrRestart %s', params, config.path));
+    return shipit.remote(util.format('%s pm2 startOrRestart %s %s', params, config.path, options))
   }
 }
